ea8ba5d2c29ff5b23ddc082ccd3f90b72ef93b1f
smlu <smlu@s5.net>
2020-05-02 20:36:31 +0200

    Fix deprecated 'scanHexInt32' warning on iOS

--- ios/Classes/SwiftFlutterNfcKitPlugin.swift
+++ ios/Classes/SwiftFlutterNfcKitPlugin.swift
@@ -22,8 +22,8 @@ func dataWithHexString(hex: String) -> Data {
         let subIndex = hex.index(hex.startIndex, offsetBy: 2)
         let c = String(hex[..<subIndex])
         hex = String(hex[subIndex...])
-        var ch: UInt32 = 0
-        Scanner(string: c).scanHexInt32(&ch)
+        var ch: UInt64 = 0
+        Scanner(string: c).scanHexInt64(&ch)
         var char = UInt8(ch)
         data.append(&char, count: 1)
     }
