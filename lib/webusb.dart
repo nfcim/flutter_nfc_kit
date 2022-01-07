f99589aa0d26289dc84dec5c4b61267f2c537695
dangfan <dangf09@gmail.com>
2022-01-07 19:21:23 +0800

    prevent polling when connected

--- lib/webusb.dart
+++ lib/webusb.dart
@@ -5,8 +5,8 @@ import 'dart:convert';
 import 'dart:js_util';
 
 import 'package:convert/convert.dart';
-import 'package:logging/logging.dart';
 import 'package:js/js.dart';
+import 'package:logging/logging.dart';
 
 final log = Logger('FlutterNFCKit:WebUSB');
 
@@ -32,32 +32,28 @@ class _USBDeviceFilter {
 @JS()
 @anonymous
 class _USBControlTransferParameters {
-  external factory _USBControlTransferParameters(
-      {String requestType,
-      String recipient,
-      int request,
-      int value,
-      int index});
+  external factory _USBControlTransferParameters({String requestType, String recipient, int request, int value, int index});
 }
 
 class WebUSB {
   static dynamic device;
 
   static Future<String> poll() async {
-    var devicePromise = _USB.requestDevice(new _USBDeviceRequestOptions(
-        filters: [new _USBDeviceFilter(classCode: 0xFF)]));
-    device = await promiseToFuture(devicePromise);
-    await promiseToFuture(callMethod(device, 'open', List.empty()));
-    await promiseToFuture(callMethod(device, 'claimInterface', [1]));
-    _USB.ondisconnect = allowInterop(onDisconnect);
+    if (device == null || getProperty(device, 'opened')) {
+      var devicePromise = _USB.requestDevice(new _USBDeviceRequestOptions(filters: [new _USBDeviceFilter(classCode: 0xFF)]));
+      device = await promiseToFuture(devicePromise);
+      await promiseToFuture(callMethod(device, 'open', List.empty()));
+      await promiseToFuture(callMethod(device, 'claimInterface', [1]));
+      _USB.ondisconnect = allowInterop(onDisconnect);
+    }
     int vendorId = getProperty(device, 'vendorId');
     int productId = getProperty(device, 'productId');
-    String id =
-        '${vendorId.toRadixString(16).padLeft(4, '0')}:${productId.toRadixString(16).padLeft(4, '0')}';
+    String id = '${vendorId.toRadixString(16).padLeft(4, '0')}:${productId.toRadixString(16).padLeft(4, '0')}';
     return json.encode({'type': 'webusb', 'id': id, 'standard': 'unknown'});
   }
 
   static Future<String> transceive(String capdu) async {
+    log.config('capdu: $capdu');
     var rapdu = '';
     do {
       if (rapdu.length >= 4) {
@@ -69,6 +65,7 @@ class WebUSB {
       }
       rapdu += await _transceive(capdu);
     } while (rapdu.substring(rapdu.length - 4, rapdu.length - 2) == '61');
+    log.config('rapdu: $rapdu');
     return rapdu;
   }
 
@@ -80,26 +77,14 @@ class WebUSB {
   static Future<String> _transceive(String capdu) async {
     // send a command
     var promise = callMethod(device, 'controlTransferOut', [
-      new _USBControlTransferParameters(
-          requestType: 'vendor',
-          recipient: 'interface',
-          request: 0,
-          value: 0,
-          index: 1),
+      new _USBControlTransferParameters(requestType: 'vendor', recipient: 'interface', request: 0, value: 0, index: 1),
       hex.decode(capdu)
     ]);
     await promiseToFuture(promise);
     // wait for execution
     while (true) {
-      promise = callMethod(device, 'controlTransferIn', [
-        new _USBControlTransferParameters(
-            requestType: 'vendor',
-            recipient: 'interface',
-            request: 2,
-            value: 0,
-            index: 1),
-        1
-      ]);
+      promise = callMethod(device, 'controlTransferIn',
+          [new _USBControlTransferParameters(requestType: 'vendor', recipient: 'interface', request: 2, value: 0, index: 1), 1]);
       var resp = await promiseToFuture(promise);
       if (getProperty(resp, 'status') == 'stalled') {
         throw Exception('device error');
@@ -114,15 +99,8 @@ class WebUSB {
       }
     }
     // get the response
-    promise = callMethod(device, 'controlTransferIn', [
-      new _USBControlTransferParameters(
-          requestType: 'vendor',
-          recipient: 'interface',
-          request: 1,
-          value: 0,
-          index: 1),
-      1500
-    ]);
+    promise = callMethod(device, 'controlTransferIn',
+        [new _USBControlTransferParameters(requestType: 'vendor', recipient: 'interface', request: 1, value: 0, index: 1), 1500]);
     var resp = await promiseToFuture(promise);
     if (getProperty(resp, 'status') != 'ok') {
       throw Exception('device error');
