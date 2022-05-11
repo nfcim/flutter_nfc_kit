e2993b89ed3283c3fae82c1d5dd7a4e4e0dc3b37
Harry Chen <i@harrychen.xyz>
2022-05-11 16:25:35 +0800

    Change function signature of MethodResultWrapper, fix #72
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- android/src/main/kotlin/im/nfc/flutter_nfc_kit/FlutterNfcKitPlugin.kt
+++ android/src/main/kotlin/im/nfc/flutter_nfc_kit/FlutterNfcKitPlugin.kt
@@ -455,7 +455,7 @@ class FlutterNfcKitPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
             }
         }
 
-        override fun error(errorCode: String?, errorMessage: String?, errorDetails: Any?) {
+        override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
             handler.post {
                 ignoreIllegalState {
                     methodResult.error(errorCode, errorMessage, errorDetails)
